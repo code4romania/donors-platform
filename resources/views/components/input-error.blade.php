fb9fc47fbe46b6ad9a533d3021492619fc167b84
Andrei Ioniță <hi@andrei.io>
2020-07-29 23:47:46 +0100

    wip

--- resources/views/components/input-error.blade.php
+++
@@ -1,5 +0,0 @@
-@error($field)
-    <span class="mt-2 text-sm text-red-600" role="alert">
-        {{ $message }}
-    </span>
-@enderror
--- routes/dashboard.php
+++ routes/dashboard.php
@@ -6,3 +6,5 @@ use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard.index');
+
+Route::resource('donors', 'DonorController');
