48e46df0645b55b39dd74fb35d5ecd144d1ab3f0
Andrei Ioniță <hi@andrei.io>
2020-08-05 16:54:52 +0100

    wip

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('locale', 5)->default(config('app.locale'));
             $table->rememberToken();
             $table->timestamps();
         });
