41ed266b385f289e585898a8b82a1c6890690b76
Andrei Ioniță <hi@andrei.io>
2020-09-19 19:40:28 +0100

    wip

--- config/dashboard.php
+++ config/dashboard.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 return [
 
-    'url'  => env('DASHBOARD_URL', 'dashboard.' . env('APP_URL')),
-    'path' => env('DASHBOARD_PATH', ''),
+    'url'  => env('DASHBOARD_URL', env('APP_URL')),
+    'path' => env('DASHBOARD_PATH', '/dashboard'),
 
 ];
