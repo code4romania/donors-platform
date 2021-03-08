3c71e6b52e7a7a0d642a35e432d1bb8306d5e785
Andrei Ioniță <hi@andrei.io>
2021-03-08 11:49:31 +0000

    fix: store only numeric tax_id

--- app/Http/Requests/GranteeRequest.php
+++ app/Http/Requests/GranteeRequest.php
@@ -11,6 +11,20 @@ use Illuminate\Validation\Rule;
 class GranteeRequest extends FormRequest
 {
     /**
+     * Prepare the data for validation.
+     *
+     * @return void
+     */
+    protected function prepareForValidation()
+    {
+        $this->merge([
+            'tax_id' => $this->tax_id !== null
+                ? preg_replace('/\D/', '', $this->tax_id)
+                : null,
+        ]);
+    }
+
+    /**
      * Get the validation rules that apply to the request.
      *
      * @return array
