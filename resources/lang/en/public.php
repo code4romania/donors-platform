eb2a9dd264c2f481af523125d496801e9691574f
Andrei Ioniță <hi@andrei.io>
2021-04-06 14:17:40 +0100

    wip

--- resources/lang/en/public.php
+++ resources/lang/en/public.php
@@ -4,13 +4,84 @@ declare(strict_types=1);
 
 return [
 
+    'title' => 'Platforma Donatorilor',
+    'description' => [
+        'Platforma Donatorilor își propune să aducă o mai mare claritate asupra investițiilor strategice în sectorul non-profit devenind astfel un punct esențial de informare cu privire la alocarea sprijinului financiar în domenii specifice de intervenție.',
+        'Dacă reprezinți o fundație, o federație sau o companie și vrei să te alături acestui demers, te invităm să ne transmiți un mesaj.',
+    ],
+
+    'what' => [
+        'subtitle' => 'Date deschise',
+        'title' => 'Ce este Platforma Donatorilor?',
+        'text' => 'Platforma Donatorilor este un proiect inițiat de o [coaliție de 15 finanțatori](/ro/about) care își deschid public datele despre granturile, finanțările și donațiile lor către proiecte derulate de societatea civilă. Ca membru al Platformei Donatorilor vei avea acces la:',
+
+        'aggregate_title' => 'Date agregate',
+        'aggregate_text' => 'Datele pe care le poți vedea mai jos sunt agregate din raportările detaliate ale finanțatorilor. Poți vedea cu ajutorul filtrelor disponibile câți bani sunt alocați pe principalele domenii de intervenție din România. Ca membru vei avea acces la un set mult mai vast de informații.',
+
+        'dashboard_title' => 'Dashboard intuitiv',
+        'dashboard_text' => 'Orice donator care se alătură platformei va putea atât să își deschidă datele cu ajutorul unui panoul de administrare simplu și intuitiv și va avea parte de sprijin permanent din partea administratorului în proces și va putea analiza datele deja existente în platformă.',
+
+        'interactive_title' => 'Vizualizare interactivă',
+        'interactive_text' => 'Datele pot fi analizate direct din platformă, sau pot fi descărcate și procesate pentru analize în profunzime. Toate informațiile prezente în platformă aparțin donatorilor, pentru informații mai detaliate veți putea contacta fiecare donator individual.',
+    ],
+
+    'chart' => [
+        'title' => 'Descoperă datele',
+        'text' => 'Descoperă mai jos toate datele disponibile până în acest moment în Platforma Donatorilor. Folosește filtrele de mai jos pentru a face comparații între sumele totale ale finanțărilor acordate pe domenii, anual sau compara între ani.',
+        'disclaimer' => 'Datele istorice din platformă sunt în curs de actualizare. Anul 2020 prezintă rezultate complete.',
+    ],
+
+    'results' => [
+        'title' => 'Rezultate',
+        'text' => 'Datele disponibile sunt rezultatul deschiderii acestor informații de către finanțatori.',
+        'first_box'         => 'Până acum, în Platforma Donatorilor există:',
+        'donors_count'      => 'finanțatori',
+        'grants_total'      => 'investiți în :domains_count de domenii',
+        'projects_count'    => 'proiecte finanțate',
+        'foundations_count' => 'fundații comunitare',
+        'grantees_count'    => 'titulari de granturi',
+    ],
+
+    'join' => [
+        'title' => 'Vino alături de noi',
+        'text' => 'Dacă vrei să te alături membrilor Platformei Donatorilor, să îți deschizi datele în platformă și sa ai acces la informații detaliate, te rugăm să ne contactezi via e-mail pentru a porni o discuție. Apasă pe butonul de mai jos și trimite-ne un mesaj.',
+    ],
+
+    'request_account' => 'Solicită cont',
+    'read_more' => 'Citește articolul',
+
     'menu' => [
         'about'   => 'About',
+        'blog'    => 'Blog',
         'contact' => 'Contact',
         'terms'   => 'Terms & conditions',
         'privacy' => 'Privacy policy',
     ],
 
+    'menu_name' => [
+        'links' => 'Linkuri utile',
+    ],
+
     'poweredby' => 'This platform has been designed, developed and is administered by',
 
+    'tag' => 'Platforma Donatorilor este prima platformă de date deschise a finanțatorilor din România.',
+
+    'pages' => [
+        'about' => [
+            'title' => 'Platforma Donatorilor',
+            'subtitle' => 'despre proiect',
+
+            'summary' => [
+                'Platforma Donatorilor își propune să aducă o mai mare claritate asupra investițiilor strategice în sectorul non-profit devenind astfel un punct esențial de informare cu privire la alocarea sprijinului financiar în domenii specifice de intervenție, identificarea de noi oportunități de finanțare în domenii încă neexplorate, crearea de mecanisme de co-finanțare și descoperirea de noi actori neguvernamentali și proiectele acestora.',
+                'Până în luna aprilie 2021, în Platforma Donatorilor s-au deschis date din partea a 15 finanțatori și 19 Fundații Comunitare, reprezentând granturi în valoare totală de 45,7 milioane de euro.',
+            ],
+
+            'founders' => 'Inițiatorii proiectului',
+            'alphabetical' => '(în ordine alfabetică)',
+        ],
+    ],
+
+    'newsletter' => [
+        'subscribe' => 'Abonează-mă',
+    ],
 ];
