e3afc8c58a5ae160be91d8540c8246e0fd50d015
Andrei Ioniță <hi@andrei.io>
2021-04-20 12:28:48 +0100

    feat: migrate deployer

--- deploy/tasks/npm.php
+++ deploy/tasks/npm.php
@@ -4,25 +4,28 @@ declare(strict_types=1);
 
 namespace Deployer;
 
+set('bin/npm', fn () => locateBinaryPath('npm'));
+
 desc('Install npm packages');
-task('assets:install', function () {
+task('npm:install', function () {
     if (test('[ -d node_modules ]')) {
         return;
     }
 
-    run('{{bin/npm}} install');
+    run('{{bin/npm}} ci --prefer-offline');
 })->local();
 
 desc('Build frontend assets locally');
-task('assets:build', function () {
+task('npm:build', function () {
     if (test('[ -d public/assets ]')) {
         return;
     }
+
     run('{{bin/npm}} run production');
 })->local();
 
 desc('Upload assets to your hosts');
-task('assets:upload', function () {
+task('npm:upload', function () {
     upload('public/assets/', '{{release_path}}/public/assets/');
     upload('public/mix-manifest.json', '{{release_path}}/public/mix-manifest.json');
 });
