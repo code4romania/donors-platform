3f86b36ec092519b72d2b72584719056239e0394
Andrei Ioniță <hi@andrei.io>
2022-10-31 14:30:52 +0000

    fix: sentry config

--- config/sentry.php
+++ config/sentry.php
@@ -7,10 +7,10 @@ return [
     'dsn' => env('SENTRY_LARAVEL_DSN', env('SENTRY_DSN')),
 
     // capture release as git sha
-    'release' => trim(exec('git --git-dir ' . base_path('.git') . ' log --pretty="%h" -n1 HEAD')),
+    // 'release' => trim(exec('git --git-dir ' . base_path('.git') . ' log --pretty="%h" -n1 HEAD')),
 
     // When left empty or `null` the Laravel environment will be used
-    'environment' => env('SENTRY_ENVIRONMENT', env('APP_ENV')),
+    'environment' => env('SENTRY_ENVIRONMENT'),
 
     'breadcrumbs' => [
         // Capture Laravel logs in breadcrumbs
@@ -29,7 +29,31 @@ return [
         'command_info' => true,
     ],
 
-    // @see: https://docs.sentry.io/error-reporting/configuration/?platform=php#send-default-pii
-    'send_default_pii' => false,
+    'tracing' => [
+        // Trace queue jobs as their own transactions
+        'queue_job_transactions' => env('SENTRY_TRACE_QUEUE_ENABLED', false),
+
+        // Capture queue jobs as spans when executed on the sync driver
+        'queue_jobs' => true,
+
+        // Capture SQL queries as spans
+        'sql_queries' => true,
+
+        // Try to find out where the SQL query originated from and add it to the query spans
+        'sql_origin' => true,
+
+        // Capture views as spans
+        'views' => true,
+
+        // Indicates if the tracing integrations supplied by Sentry should be loaded
+        'default_integrations' => true,
+    ],
+
+    // @see: https://docs.sentry.io/platforms/php/configuration/options/#send-default-pii
+    'send_default_pii' => env('SENTRY_SEND_DEFAULT_PII', false),
+
+    'traces_sample_rate' => (float)(env('SENTRY_TRACES_SAMPLE_RATE', 0.0)),
+
+    'controllers_base_namespace' => env('SENTRY_CONTROLLERS_BASE_NAMESPACE', 'App\\Http\\Controllers'),
 
 ];
